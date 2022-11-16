948051b6970244844e7d80a988f8ede9bf4579a0
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-11-16 03:24:26 -0800

    swapping program

---
+++ SWAPPING.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.>
+void main()
+int num1,num2,swp;
+clrscr();
+printf("enter the two no")
+scanf("%d%d",&num1,&num2);
+printf("the before swapping\n num1=%d num2=%d,num1,num2);
+swp=num2;
+num2=num1;
+num1=swp;
+printf("\nthe after swapping\ num1=%d",num1,num2);
+getch();
+}
