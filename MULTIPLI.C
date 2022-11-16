f1f31fa2e3784eccf52686cb45887db40b815c52
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-11-16 08:57:39 -0800

    multiplaction of two no.

---
+++ MULTIPLI.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2,result;
+clrscr();
+printf("enter the two no");
+scanf("%d%d,&num1,&num2);
+result=num1*num2;
+printf("multiplication of two no=%d",result);
+getch();
+}
