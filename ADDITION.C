7aaa7774fa839726a023d6e1246d87ba67bad401
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-11-16 02:48:32 -0800

    WAP additon of two no.

---
+++ ADDITION.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int mum1,num2,result;
+clrscr();
+printf("enter the two numbers");
+scanf("%d%d",&num1,num2);
+result=num1+num2;
+printf("the sum of two number= %d",result);
+getch()
+}
+
+
+
+
+
+
+
+
+
+
+
