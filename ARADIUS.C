d6bb1b8e426fbeea478914e2b595e7cb3061e1b8
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-11-16 10:17:32 -0800

    area of circle

---
+++ ARADIUS.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int r,area;
+clrscr();
+printf("enter the radius of circle");
+scanf("%d",&r);
+area=3.14*r*r;
+printf("radius of circle %d",area);
+getch();
+}
