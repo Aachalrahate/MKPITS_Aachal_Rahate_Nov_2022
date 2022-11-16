f4063ea7169f2e7fd614c516137a486ed84412f5
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-11-16 11:05:39 -0800

    area of reactangle

---
+++ RECTANGL.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+float lenth,breath,area;
+
+clrscr();
+printf("enter the area of rectangle");
+scanf("%f",&rectangle);
+area=lenth*breath;
+printf("area of rectangle %f",area);
+getch();
+}
