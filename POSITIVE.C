dbee2bff139386436767e8f9d2450e3ccdd42fb4
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-11-17 08:58:15 -0800

    positive or negative

---
+++ POSITIVE.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+clrscr();
+printf("enter the num");
+scanf("%d",&num);
+if(num>0)
+{
+printf("the no. is positive");
+}
+else
+{
+printf("the no. is negative");
+}
+getch();
+}
