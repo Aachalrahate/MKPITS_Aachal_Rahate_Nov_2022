9aaefc02def2f9a7902d0edd8fc1c3b29f26a278
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-11-21 01:32:55 -0800

    array 1

---
+++ array1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[4];
+	a[0]=2;
+	a[1]=3;
+	a[2]=4;
+    a[3]=6;
+    a[4]=8;
+    {
+    	printf(" \n %d \n %d \n %d \n %d \n %d",a[0],a[1],a[2],a[3],a[4]);
+	}
+	getch();
+}
