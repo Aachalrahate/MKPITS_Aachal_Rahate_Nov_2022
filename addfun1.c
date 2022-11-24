c28a24d1898f62c00f85f4b503cb4adc39e722bc
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-11-24 02:52:27 -0800

    No return no argument

---
+++ addfun1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void add()
+{
+	int num1,num2,result;
+	printf("enter the two no");
+	scanf("%d%d",&num1,&num2);
+	result=num1+num2;
+	
+		printf("addition no=%d",result);
+}
+	
+	void main()
+	{
+		add();
+		getch();
+	}
+
