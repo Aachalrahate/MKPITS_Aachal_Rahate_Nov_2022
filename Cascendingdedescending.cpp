cdab6ae0b7893b41c9b1832f7d12d9332b7f2859
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-11-29 20:22:57 +0530

    check if two no in pair is in asce or desc  order

---
+++ Cascendingdedescending.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x,y,i,total=0;
+	cout<<"\n input a pair of no";
+	cout<<"\n input first no of pair";
+	cin>>x;
+	cout<<"\n input second no of pair";
+	cin>>y;
+	if(x>y)
+	{
+		cout<<"the pair is in descending ordre";
+		
+	}
+	else
+	{
+		cout<<"the pair is in ascending order";
+	}
+	cout<<"\n";
+}
