ae79e9ca695159775d21c592eff1f73b59d6cf45
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-11-29 23:18:20 +0530

    lines in a sequance of 1tob in a line

---
+++ sequence1tob.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int x,y,i,j,l;
+	printf("input no of line");
+	scanf("%d",&x);
+	printf("no of in line");
+	scanf("%d",&y);
+	for(i=1;i<=x;i++)
+	{
+		for(j=1;j<=y;j++)
+		{
+		printf("%d",l);
+		i++;
+		}
+		printf("\n");
+	}
+}
