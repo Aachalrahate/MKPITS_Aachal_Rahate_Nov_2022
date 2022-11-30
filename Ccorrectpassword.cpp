43f1909df4975b1bde72cda4fb4e0bf5eec5c294
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-11-30 12:51:06 +0530

    correct password is 1234

---
+++ Ccorrectpassword.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"print the coreect password";
+	cin>>x;
+	if(x=1234)
+	{
+		cout<<"correct password";
+	}
+	else
+	{
+		cout<<"incorrect password";
+	}
+}
