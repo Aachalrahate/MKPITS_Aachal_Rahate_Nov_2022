e6b61deae44f0b73f3df08346c32ce9ecbc370cf
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-12-12 11:50:49 +0530

    Override Inheritance

---
+++ override.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class one
+{
+	public:
+	int dis()
+	{
+		cout<<"one";
+	}
+};
+class two
+{
+	public:
+	int dis()
+	
+		cout<<"two";
+	}
+};
+int main()
+{
+	two ob;
+	ob.dis();
+}
