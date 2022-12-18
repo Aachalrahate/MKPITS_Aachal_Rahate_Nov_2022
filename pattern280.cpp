2aea6cc96a20eecfcc1718ef7d97c71bf44850ae
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2022-12-18 23:47:50 +0530

    Pattern program280

---
+++ pattern280.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a=65,i,j;
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(j==1)
+			{
+				printf("%c",a);
+			}
+			else
+			{
+				printf("%c",a+32);
+			}
+		
+		}
+		a++;
+		{
+		printf("\n");
+	}
+	}
+}
+	
+	
+
