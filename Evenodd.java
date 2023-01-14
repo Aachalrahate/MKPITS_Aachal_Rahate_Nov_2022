dde15233bccb5d8b147a28d4cf206f2c563a251d
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-14 16:09:34 +0530

    Evenodd_if else (Java)
    Java

---
+++ Evenodd.java
@@ -0,0 +1,16 @@
+class Evenodd
+{
+public static void main(String[]args)
+{
+int num=3;
+System.out.println("enter the no");
+if(num%2==0)
+{
+System.out.println("The number is even");
+}
+else
+{
+System.out.println("The number is odd");
+}
+}
+}
