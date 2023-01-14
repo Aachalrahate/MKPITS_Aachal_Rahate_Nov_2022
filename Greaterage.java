b1c731439e23c715090c679dc8b3867a9f31f369
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-14 17:16:38 +0530

    Greater age_Nested if else (Java)
    Java
    Nested if else

---
+++ Greaterage.java
@@ -0,0 +1,22 @@
+class Greaterage
+{
+public static void main(String[] args)
+{
+int a1=21,a2=33,a3=45;
+System.out.println("Enter the age");
+if(a1>a2 && a1>a3)
+{
+System.out.println("a1 is greater age");
+}
+else
+if(a2>a3 && a2>a1)
+{
+System.out.println("a2 is greater age");
+}
+else
+if(a3>a1 && a3>a2)
+{
+System.out.println("a3 is greater age");
+}
+}
+}
