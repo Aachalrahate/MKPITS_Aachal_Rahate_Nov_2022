8e96afa534dc4d2ef4319b0b382f8412bcd65fbb
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-14 15:35:49 +0530

    Radius(Java)
    JAVA
    Formulas

---
+++ Radius.java
@@ -0,0 +1,10 @@
+class Radius
+{
+public static void main(String[] args)
+{
+ int r=3;
+double res=3.14*(r*r);
+System.out.println("Enter the Radius of circle");
+System.out.println("radius=" +res);
+}
+}
