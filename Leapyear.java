96a20ce21484e245f17fb056022722691c835738
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-14 16:07:21 +0530

    Leapyear_if else (Java)
    Java

---
+++ Leapyear.java
@@ -0,0 +1,16 @@
+class Leapyear
+{
+public static void main(String[]args)
+{
+int year=2020;
+System.out.println("enter year");
+if(year%4==0)
+{
+System.out.println("It is leap year");
+}
+else
+{
+System.out.println("It is not a leap year");
+}
+}
+}
