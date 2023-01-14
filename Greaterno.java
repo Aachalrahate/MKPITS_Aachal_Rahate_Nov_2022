0c4019d84d65edc3e0b538df077cf50ceadfb5a4
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-14 16:24:06 +0530

    Greater Number_nested if else (Java)
    Nested if else
    Java

---
+++ Greaterno.java
@@ -0,0 +1,22 @@
+class Greaterno
+{
+public static void main(String[] args)
+{
+int p1=2,p2=3,p3=4;
+System.out.println("Enter the Edge of 3 no");
+if(p1>p2 && p1>p3)
+{
+System.out.println("p1 is greater");
+}
+else
+if(p2>p3 && p2>p1)
+{
+System.out.println("p2 is greater");
+}
+else
+if(p3>p1 && p3>p2)
+{
+System.out.println("p3 is greater");
+}
+}
+}
