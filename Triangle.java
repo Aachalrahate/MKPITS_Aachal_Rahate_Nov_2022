8bb88f5ef8d0090ec1755eb3149b6f86ca4fa9a0
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-14 15:32:40 +0530

    Area of triangle(Java)
    Java(formulas)

---
+++ Triangle.java
@@ -0,0 +1,11 @@
+class Triangle
+{
+public static void main(String[] args)
+{
+int l=2,b=3;
+float res;
+res=0.5f*l*b;
+System.out.println("area of rectangle");
+System.out.println("Triangle=" +res);
+}
+}
