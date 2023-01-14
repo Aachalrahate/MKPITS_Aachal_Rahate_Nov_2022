2b74b387be13859db161113f251ec6b18dd6a326
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-15 00:41:26 +0530

    Pattern2 (Java)

---
+++ Pattern2.java
@@ -0,0 +1,15 @@
+class Pattern2
+{
+public static void main(String[] args)
+{
+int i,j,n=5;
+	for(i=1;i<=n;i++)
+	{
+		for(j=i;j<=i;j++)
+		{
+	System.out.print("* ");
+		}
+		System.out.println("");
+	}
+	}
+	}
