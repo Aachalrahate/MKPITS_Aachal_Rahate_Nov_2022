ddb42dfc997a1a35e02698df7c9b3573a5451de2
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-17 22:52:19 +0530

    Typecasting in Java
    Typecasting in Java

---
+++ Typecasting.java
@@ -0,0 +1,16 @@
+public class Typecasting
+{
+public static void main(String[] args)
+{
+byte a=2;
+short s=a;
+//impicit casting byte to short
+long l1=12;
+//explit cast long to byte
+a=(byte)l1;
+System.out.println(s);
+System.out.println(a);
+
+
+}
+}
