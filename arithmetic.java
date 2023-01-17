b8a1167206bf30dab0c776852362e5facc161e14
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-17 23:11:50 +0530

    Arithmatic series in Java
    1 4 7 10 13 16 19.....n

---
+++ arithmetic.java
@@ -0,0 +1,20 @@
+class arth
+{
+int i;
+void set()
+{
+for(i=1;i<=30;i=i+3)
+{
+System.out.println(i);
+}
+}
+}
+
+class arithmetic
+{
+public static void main(String[] args)
+{
+arth ob1=new arth();
+ob1.set();
+}
+}
