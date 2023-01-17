e2479030f3c5f641755aefb113d28b9cd89a4cf6
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-17 22:58:32 +0530

    Using Class & Object in (Java)
    addition of two object

---
+++ one.java
@@ -0,0 +1,26 @@
+ class adddemo
+{
+int a=8,b=8;
+int res;
+
+void sum()
+{
+res=a+b;
+}
+void dis()
+{
+System.out.println(res);
+}
+}
+
+ public class one
+{
+public static void main(String[] args)
+{
+adddemo ob=new adddemo();
+ob.sum();
+ob.dis();
+
+
+}
+}
