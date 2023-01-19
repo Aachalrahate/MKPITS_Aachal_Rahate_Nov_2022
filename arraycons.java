80b015bae14de92c4f39d4a8efeb2c84475ec5c4
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-19 18:20:35 +0530

    Constant Array in Java
    Array Constant

---
+++ arraycons.java
@@ -0,0 +1,21 @@
+class arraydemo
+{
+void fun()
+{
+int a[]={1,2,3,4};
+int []b;
+for(int i=0;i<=3;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
+
+public class arraycons
+{
+public static void main(String[] args)
+{
+arraydemo ob=new arraydemo();
+ob.fun();
+}
+}
