109d9359fe13919a316cd18f1c6ff56108d407f1
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-24 18:02:55 +0530

    Name String in Java

---
+++ Sname.java
@@ -0,0 +1,14 @@
+public class Sname   //convert String to Character
+{
+public static void main(String[] args)
+{
+String s="aachal";
+int i;
+char ch;
+for(i=0;i<6;i++)
+{
+ch=s.charAt(i);
+System.out.println(ch);
+}
+}
+}
