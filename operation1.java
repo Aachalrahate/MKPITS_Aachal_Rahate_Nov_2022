640c98dd61a3393fe1f044d803b12197ca497504
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-25 12:38:21 +0530

    Assign_Q1_19 jan in Java

---
+++ operation1.java
@@ -0,0 +1,22 @@
+class operation1
+{
+public static void main(String[] args)
+{
+int x;
+System.out.println("x  "+"x+2  "+"x+4  "+"x+6 ");
+for(x=1;x<=13;x=x+3)
+{
+int res=x;
+int res1=x+2;
+int res2=x+4;
+int res3=x+6;
+
+System.out.print(res);
+System.out.print(" "+res1);
+System.out.print(" "+res2);
+System.out.print(" "+res3);
+
+System.out.println(" ");
+}
+}
+}
