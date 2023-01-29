3a4f13f768eb2b9c63ce6004ac7c9188f324d1f0
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-29 23:35:02 +0530

    AssignQ1_Jan25 in java

---
+++ AssigQ1.java
@@ -0,0 +1,21 @@
+class AssigQ1
+{
+public static void main(String[] args)
+{
+int a[]={1,2,3};
+int b[]={4,5,6};
+int sum1,sum2;
+sum1=(a[0]+a[1]+a[2]);
+System.out.println("addition of 1st array=" +sum1);
+sum2=(b[0]+b[1]+b[2]);
+System.out.println("addition of 2nd array=" +sum2);
+if(sum1>sum2)
+{
+System.out.println("sum1 is greater=" +sum1);
+}
+else
+{
+System.out.println("sum2 is greater=" +sum2);
+}
+}
+}
