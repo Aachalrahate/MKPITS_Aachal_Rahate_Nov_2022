05855acf8163a6295c15f19903264c5288936925
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-01-30 17:28:04 +0530

    AssigQ12_19Jan in Java

---
+++ assigQ12.java
@@ -0,0 +1,27 @@
+/*Write a program to check whether three given integer values
+are in the range 20..50 inclusive. Return true if 1 or more of
+them are in the said range otherwise return false.*/
+
+import java.util.Scanner;
+public class assigQ12
+{
+	public static boolean value()
+	{
+		Scanner sc=new Scanner(System.in);
+		boolean check = false;
+		System.out.println("Enter the three integer numbers");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		if(a>=20 && a<=50 || b>=20 && b<=50 || c>=20 && c<=50 )
+		{
+			check=true;
+		}
+		return check;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(value());
+	}
+}
