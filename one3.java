9ecfaadac68cce8c5fdc13d8332717aed26cc3a3
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-02-19 23:56:42 +0530

    using object and class in java

---
+++ one3.java
@@ -0,0 +1,24 @@
+ class divdemo
+{
+int a=18,b=2;
+int res;
+
+void div()
+{
+res=a/b;
+}
+void dis()
+{
+System.out.println(res);
+}
+}
+
+ public class one3
+{
+public static void main(String[] args)
+{
+divdemo ob=new divdemo();
+ob.div();
+ob.dis();
+}
+}
