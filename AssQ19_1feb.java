925d2648b75a2bbe1d88620a4b1e0e08f5982f85
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-02-26 23:23:59 +0530

    Assignment Que19_1feb in java

---
+++ AssQ19_1feb.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class AssQ19_1feb
+{
+public static void main(String[] args)
+ {
+ String as;
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter the special character");
+ as=sc.nextLine();
+ }
+}
