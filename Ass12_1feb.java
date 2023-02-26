be7db6c10334ad53a87dfb5f64a35d76de823494
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-02-26 23:20:18 +0530

    Assignment Que12_1feb

---
+++ Ass12_1feb.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Ass12_1feb
+{
+public static void main(String[] args)
+{
+int a,b;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the value of a & b");
+a=sc.nextInt();
+b=sc.nextInt();
+if(a>0 && b>0)
+{
+System.out.println("1st quadrant");
+}
+else if(a>0 && b<0)
+{
+System.out.println("2st quadrant");
+}
+else if(a<0 && b<0)
+{
+System.out.println("3st quadrant");
+}
+else if(a<0 && b>0)
+{
+System.out.println("1st quadrant");
+}
+}
+}
