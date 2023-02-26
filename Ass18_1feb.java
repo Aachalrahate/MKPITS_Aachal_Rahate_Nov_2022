3caa3899e53ec4d2810ef646d724c604755b5c86
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-02-26 23:21:58 +0530

    Assignment Que18_1feb in java

---
+++ Ass18_1feb.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Ass18_1feb
+{
+public static void main(String[] args)
+{
+int a1,a2,a3;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the side1 triangle");
+a1=sc.nextInt();
+System.out.println("enter the side2 triangle");
+a2=sc.nextInt();
+System.out.println("enter the side3 triangle");
+a3=sc.nextInt();
+if(a1==a2 && a2==a3)
+  {
+  System.out.println("it is euailater triangle");
+  }
+    else if(a1==a2 || a2==a3 || a3==a1)
+     {
+     System.out.println("it is isosceles");
+     }
+       else
+         {
+         System.out.println("it is Scalene");
+         }
+}
+}
