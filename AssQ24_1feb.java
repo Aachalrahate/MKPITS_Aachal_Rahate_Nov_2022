f69362bfe916a8eae300306645813e4f328613bb
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-02-26 23:24:29 +0530

    Assignment Que24_1feb in java

---
+++ AssQ24_1feb.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class AssQ24_1feb
+{
+public static void main(String[] args)
+  {
+   int daynum;
+   Scanner sc=new Scanner(System.in);
+   System.out.println("enter the number of day");
+   daynum=sc.nextInt();
+     switch(daynum)
+       {
+         case 1:
+           System.out.println("Monday");
+           break;
+
+         case 2:
+            System.out.println("Tuesday");
+            break;
+
+         case 3:
+	    System.out.println("Wednesday");
+            break;
+
+          case 4:
+            System.out.println("Thursday");
+            break;
+
+          case 5:
+	    System.out.println("Friday");
+            break;
+
+          case 6:
+            System.out.println("Saturday");
+            break;
+
+          case 7:
+	    System.out.println("Sunday");
+            break;
+            default:
+            System.out.println("Invalid day number "+"please try next again");
+            break;
+       }
+  }
+}
