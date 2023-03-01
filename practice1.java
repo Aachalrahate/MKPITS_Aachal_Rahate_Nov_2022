9545607834cd4fc388fd13d32ed27932bcd9b8a6
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-01 19:51:39 +0530

    prime number or not in java

---
+++ practice1.java
@@ -0,0 +1,22 @@
+class practice1
+{
+public static void main(String[] args)
+ {
+ int count=0,i;
+ for(i=2;i<10;i++)
+   {
+   if(i%2!=0)
+    {
+    count++;
+    }
+    if(count==1 && count==0)
+     {
+     System.out.println("It is prime number");
+     }
+     else
+     {
+      System.out.println("It is not prime number");
+     }
+   }
+ }
+}
