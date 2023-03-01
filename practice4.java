261b97c7bfaec0776ac813ea4a0db84fdc2b98c5
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-01 19:54:06 +0530

    string length in aaray

---
+++ practice4.java
@@ -0,0 +1,17 @@
+class practice4
+{
+public static void main(String[] args)
+ {
+ String str="Aachal";
+ int d=str.length()/2;
+ System.out.println(d);
+ String s1=str.substring(0,d);
+ System.out.println(s1);
+ String s2=str.substring(4);
+  System.out.println(s2);
+  String s3=str.toUpperCase();
+   System.out.println(s3);
+   String s4=str.toLowerCase();
+    System.out.println(s4);
+ }
+}
