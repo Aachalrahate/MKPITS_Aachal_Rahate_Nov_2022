f48279913d0dcced41376f68ea5d079ff8ac8aa7
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-02 23:36:06 +0530

    positive Array in java

---
+++ prac12.java
@@ -0,0 +1,15 @@
+class prac12
+   {
+   public static void main(String[] args)
+    {
+    int a[]={34,-9,5,-2,55,67,-78};
+    System.out.println("positive number in aaray");
+    for(int i=0;i<a.length;i++)
+       {
+       if(a[i]>0)
+        {
+         System.out.println(a[i]);
+        }
+       }
+    }
+   }
