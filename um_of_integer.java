e538ea6af421732515680369d97285f2830f7052
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-02 23:37:36 +0530

    sum of integer in java

---
+++ um_of_integer.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+ class sum_of_integer
+  {
+  public static void main(String[] args)
+    {
+
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the 1st integer");
+    int num1=sc.nextInt();
+
+    System.out.println("enter the 2st integer");
+    int num2=sc.nextInt();
+
+   int sum=num1+num2;
+
+    System.out.println(sum);
+    }
+  }
