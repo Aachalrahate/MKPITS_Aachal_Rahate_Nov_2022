910aaf2aa45a93545f72b9222c8d80d3dea2e21f
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-02 23:35:23 +0530

    nagative array in java

---
+++ prac11.java
@@ -0,0 +1,15 @@
+class prac11
+  {
+  public static void main(String[] args)
+    {
+     int a[]={1,-3,4,8,5,-8,6};
+     System.out.println("nagative value");
+      for(int i=0;i<a.length;i++)
+        {
+        if(a[i]<0)
+        {
+        System.out.println(a[i]);
+        }
+        }
+    }
+  }
