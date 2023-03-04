698ee32b185f714fe53ff5e4726618aa9bcf7a88
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-04 13:54:28 +0530

    practice que

---
+++ prac13.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class prac13
+  {
+  public static void main(String[] args)
+     {
+	  int a,b;
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter your number");
+      a=sc.nextInt();
+      b=sc.nextInt();
+     int sum=a+b;
+     System.out.println("sum of two num" +sum);
+     }
+  }
