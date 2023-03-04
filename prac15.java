d4bbd4fed12809af6353316611b288199a6ba772
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-04 13:56:27 +0530

    Area of rectangle in java

---
+++ prac15.java
@@ -0,0 +1,13 @@
+class prac15{
+public static void main(String[] args)
+ {
+ int h=7;
+ int w=5;
+ System.out.println("perimeter of rectangle");
+ int p=2*(h+w);
+ System.out.println("perimeter=" +p);
+ System.out.println("Area of rectangle");
+ int a=w*h;
+ System.out.println("Area=" +a);
+ }
+}
