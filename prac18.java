be03f12daaf41373c37c3850549124fb081f3be3
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-04 13:59:37 +0530

    print N to 1 number in java

---
+++ prac18.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class prac18{
+ public static void main(String[] args)
+   {
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter the Number");
+   int n=sc.nextInt();
+   for(int i=n;i>=1;i--)
+     {
+     System.out.println(i);
+     }
+   }
+}
