b56f350a12538d0de12d4ca6c6072d02d7caa81c
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-05 16:03:20 +0530

    Swap number in java

---
+++ prac31.java
@@ -0,0 +1,15 @@
+class prac31
+  {
+  public static void main(String[] args)
+    {
+    int i=10;
+    int j=20;
+     i=i+j;
+     j=i-j;
+     i=i-j;
+     
+     System.out.println("i="+i);
+     System.out.println("j="+j);
+
+    }
+  }
