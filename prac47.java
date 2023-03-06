4b3e9e23e1f77ca649caaac0efffa28313da7e4d
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-06 14:49:24 +0530

    Pattern program in java

---
+++ prac47.java
@@ -0,0 +1,14 @@
+class prac47
+  {
+  public static void main(String args[])
+   {
+   for(int r=4;r<=1;r++)
+    {
+    for(int c=1;c<=r;c++)
+      {
+      System.out.print(r);
+      }
+      System.out.println(" ");
+    }
+   }
+  }
