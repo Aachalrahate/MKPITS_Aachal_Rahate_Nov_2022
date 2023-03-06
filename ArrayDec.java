e0ba26b88bd0912ef5641692ca907a54d942bd5d
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-06 21:42:19 +0530

    Array Decending Number in java

---
+++ ArrayDec.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class ArrayDec
+  {
+  public static void main(String args[])
+   {
+   int [] a=new int[]{9,5,3,67,34,12,54,98};
+   Arrays.sort(a);
+   System.out.println("Array of Decending order");
+   for(int i=0;i>a.length;i--)
+    {
+    System.out.println(a[i]);
+    }
+   }
+  }
