f0601fbcc8d6fcd7c1bf1cac02e91acae696bb4b
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-06 14:52:48 +0530

    Add files via upload

---
+++ prac48.java
@@ -0,0 +1,14 @@
+class prac48
+ {
+ public static void main(String args[])
+  {
+  for(int r=4; r>=1;r--)
+   {
+   for(int c=1;c<=r;c++)
+     {
+     System.out.print("*");
+     }
+      System.out.println(" ");
+   }
+  }
+ }
