a52540347ac44845bc7eeb646c42eaeac4010415
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-06 14:36:16 +0530

    pattern program

---
+++ prac45.java
@@ -0,0 +1,14 @@
+class prac45
+  {
+  public static void main(String args[])
+    {
+    for(int r=1; r<=4;r++)
+     {
+     for(int c=1;c<=3;c++)
+       {
+       System.out.print(r);
+       }
+       System.out.println(" ");
+     }
+    }
+  }
