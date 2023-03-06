ef78464fffbefa5070d76be146e52607603ab432
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-06 14:32:31 +0530

    practice que

---
+++ prac43.java
@@ -0,0 +1,18 @@
+
+class prac43
+ {
+ public static void main(String args[])
+   {
+   int n1=0,n2=1,n3,i,n=10;
+   System.out.println(n1+" "+n2);
+
+   for(i=2;i<n;i++)
+    {
+    n3=n1+n2;
+    System.out.println(" "+n3);
+
+    n1=n2;
+    n2=n3;
+    }
+   }
+ }
