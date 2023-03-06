5c46bb23e8dd76b89ffe4d9d8635af5e796364f7
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-06 21:38:54 +0530

    Subtraction of two matrix in java

---
+++ SubTwoMatrix.java
@@ -0,0 +1,19 @@
+class SubTwoMatrix
+ {
+ public static void main(String args[])
+  {
+  int a[][]={ {1,2,3},{2,5,1},{6,4,2} };
+  int b[][]={ {1,1,1},{5,3,2},{8,4,1} };
+  int c[][]=new int [3][3];
+
+  for(int i=0;i<3;i++)
+   {
+   for(int j=0;j<3;j++)
+     {
+     c[i][j]=a[i][j]-b[i][j];
+     System.out.print(" "+c[i][j]);
+     }
+     System.out.println();
+   }
+  }
+ }
