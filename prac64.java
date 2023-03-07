8f5ed4af931b746599f5666e1743f089e3a124a3
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-07 23:42:16 +0530

    Priority Queue in java

---
+++ prac64.java
@@ -0,0 +1,19 @@
+import java.util.PriorityQueue;
+
+class prac64
+ {
+	 public static void main(String[] args)
+  {
+  PriorityQueue pq=new PriorityQueue();
+  System.out.println("size" +pq.size());
+  pq.add(34);
+  pq.add(5);
+  pq.add(13);
+  pq.add(9);
+  pq.add(88);
+  pq.add(50);
+  System.out.println("size" +pq.size());
+  System.out.println(pq);
+
+  }
+ }
