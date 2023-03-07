8567d9b9edd903abb77339ea9b15089a597f8221
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-07 23:44:00 +0530

    TreeMap operation in java

---
+++ prac62.java
@@ -0,0 +1,18 @@
+import java.util.TreeMap;
+
+class prac62
+ {
+ public static void main(String[] args)
+   {
+   TreeMap tm=new TreeMap();
+   System.out.println("size" +tm.size());
+   tm.put(1,"Rohan");
+   tm.put(2,"titu");
+   tm.put(3,"ram");
+   tm.put(4,"sita");
+
+  System.out.println("size" +tm.size());
+  System.out.println(tm);
+
+   }
+ }
