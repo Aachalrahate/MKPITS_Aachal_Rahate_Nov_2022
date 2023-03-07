a923be94c9c709c1586788bd497ab08086f4c3d8
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-07 23:41:16 +0530

    Stack Operation in java

---
+++ prac65.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+class prac65
+ {
+ public static void main(String[] args)
+  {
+  Stack s1=new Stack();
+  System.out.println("size" +s1.size());
+
+  s1.push(13);
+  s1.push(29);
+  s1.push(44);
+  s1.push(23);
+  s1.push(55);
+    System.out.println("size" +s1.size());
+  System.out.println(s1);
+
+  }
+ }
