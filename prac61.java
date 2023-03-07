24b0ee699f2705ca10ce311567db175569fc2d2a
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-07 23:44:35 +0530

    HasMap in java

---
+++ prac61.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+class prac61
+ {
+ public static void main(String[] args)
+   {
+   HashMap hm=new HashMap();
+   System.out.println("size" +hm.size());
+   hm.put(1,"Rohan");
+   hm.put(2,"titu");
+   hm.put(3,"ram");
+   hm.put(4,"sita");
+
+  System.out.println("size" +hm.size());
+  System.out.println(hm);
+
+   }
+ }
