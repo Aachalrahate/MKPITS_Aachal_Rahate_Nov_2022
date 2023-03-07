ffa04fc98ed0a539708270c16e128fdef8b728f8
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-07 23:45:11 +0530

    ArrayList in java

---
+++ prac60.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class prac60
+ {
+ public static void main(String args[])
+  {
+  ArrayList al=new ArrayList();
+  System.out.println("size" +al.size());
+  al.add("Ram");
+  al.add(30);
+  al.add("Sita");
+  al.add(25);
+
+  System.out.println("size" +al.size());
+  System.out.println(al);
+  }
+ }
