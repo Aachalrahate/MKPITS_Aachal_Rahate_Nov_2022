a3f16a5efbc10a07a440ad525fc66c9912d769d6
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-08 23:57:36 +0530

    Practice prb

---
+++ prac67.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+ class prac67
+   {
+   public static void main(String args[])
+     {
+      LinkedList li=new LinkedList();
+      System.out.println("Size"+ li.size());
+      li.add("prachi");
+      li.add("sahi");
+      li.add("Mahi");
+      li.add("Diya");
+      li.add("Arohi");
+      li.add("Adhira");
+
+      System.out.println("Size" +li.size());
+      System.out.println(li);
+     }
+   }
