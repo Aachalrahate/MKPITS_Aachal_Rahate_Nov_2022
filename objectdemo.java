5a7f020a0c436527ae95f59e2be1e1509222c128
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-09 22:45:51 +0530

    class and object

---
+++ objectdemo.java
@@ -0,0 +1,19 @@
+class pen
+ {
+ String color;
+ String type;
+
+ void write()
+  {
+  System.out.println("Write something");
+  }
+ }
+
+ public class objectdemo
+  {
+  public static void main(String[] args)
+   {
+    pen p1=new pen();
+    p1.write();
+   }
+  }
