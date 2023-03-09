62725ccfa9636f924cd38f022358224c20c8eda7
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-09 22:47:34 +0530

    class and object practice prb in java

---
+++ clsdemo.java
@@ -0,0 +1,21 @@
+class Animal
+ {
+ String color;
+ String type;
+
+ void work()
+  {
+  System.out.println("Eating something");
+  }
+ }
+
+ public class clsdemo
+  {
+  public static void main(String[] args)
+   {
+    Animal a1=new Animal();
+   // pen1.color="blue";
+   // pen1.type="gel";
+    a1.work();
+   }
+  }
