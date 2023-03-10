7a663e863c2548078ccb79db3d8059e9a08ecc19
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-10 23:00:19 +0530

    Reverse Array in java

---
+++ prac90.java
@@ -0,0 +1,18 @@
+class prac90
+ {
+  public static void main(String[] args)
+    {
+     int a[]={2,3,4,6,7,8,9};
+     int n=6;
+     for(int i=0;i<n;i++)
+      {
+       System.out.println(a[i]);
+      }
+      //System.out.println("Reverse array");
+
+      for(int i=n;i>=0;i--)
+       {
+        System.out.println(a[i]);
+       }
+    }
+ }
