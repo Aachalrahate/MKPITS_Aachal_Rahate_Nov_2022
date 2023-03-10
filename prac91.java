22f260d5e3b584c122dcff8e4d82fbcd1ba0babc
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-03-10 22:58:08 +0530

    practice prb

---
+++ prac91.java
@@ -0,0 +1,12 @@
+class prac91
+ {
+  public static void main(String[] args)
+   {
+    int sum=0;
+    for(int i=1;i<10;i++)
+     {
+      sum=sum+1;
+     }
+     System.out.println(sum);
+   }
+ }
