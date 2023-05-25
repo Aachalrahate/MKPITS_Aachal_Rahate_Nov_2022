4e30b7eed2386242e5d3ef8ec0bd46b7b603cd4c
Aachal Rahate <aachalrahate4@gmail.com>
2023-05-26 00:14:30 +0530

    spring boot project

---
+++ Treding/Trading/src/test/java/com/mkpits/Trading/TradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkpits.Trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
